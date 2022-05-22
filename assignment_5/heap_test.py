f509f755a1cff1b6de2917ae7ead717b14816e62
git-ginwook <leeginw@oregonstate.edu>
2022-05-22 11:53:56 +0900

    assignment 5 base

---
+++ assignment_5/heap_test.py
@@ -0,0 +1,12 @@
+import unittest
+from min_heap import *
+
+
+class TestMinHeap(unittest.TestCase):
+    """ """
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
